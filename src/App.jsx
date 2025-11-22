e2bf9269751a8c645a48b7053412228e19a6be06
SanketLV <sanketlakhani80@gmail.com>
2025-11-22 19:06:13 +0530

    Add Navbar component and dayjs dependency; update Vite config and app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "#components/Navbar";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-2xl">Welcome to my MacFolio</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
